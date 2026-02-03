8da2052dc6723c87705a730abdebae941037d2cd
Stavrolet <vladneh21@gmail.com>
2026-02-03 22:12:57 +0200

    math methods and operators added to Vector2

--- ByteEngineD3D11/Code/Include/Math/Vector2.h
+++ ByteEngineD3D11/Code/Include/Math/Vector2.h
@@ -1,6 +1,8 @@
 ﻿#pragma once
 
-namespace ByteEngine
+#include "Math/Math.h"
+
+namespace ByteEngine::Math
 {
     struct Vector2
     {
@@ -22,10 +24,73 @@ namespace ByteEngine
         };
 
         explicit constexpr Vector2(float xy = 0.0f)
-            : x(xy), y(xy) { }
+            : x(xy), y(xy)
+        { }
 
         constexpr Vector2(float x, float y)
-            : x(x), y(y) { }
+            : x(x), y(y)
+        { }
+
+        float Length() const { return Math::Sqrt(x * x + y * y); }
+        constexpr float LengthSquared() const { return x * x + y * y; }
+
+        void Normalize();
+        Vector2 Normalized() const;
+        constexpr bool IsNormalized() const { return Math::IsEqualApproximetly(1.0f, LengthSquared(), Math::UnitSizeEpsilon); }
+
+        constexpr void RotateBy(RadiansF angle)
+        {
+            float sin, cos;
+            Math::SinCos(sin, cos, angle);
+            float oldX = x;
+            x = oldX * cos - y * sin;
+            y = oldX * sin + y * cos;
+        }
+
+        constexpr Vector2 RotatedBy(RadiansF angle) const
+        {
+            Vector2 copy = *this;
+            copy.RotateBy(angle);
+            return copy;
+        }
+
+        void LimitLength(float maxLength = 1.0f);
+
+        static RadiansF AngleBetween(Vector2 from, Vector2 to);
+        static RadiansF UnsigedAngleBetween(Vector2 from, Vector2 to);
+
+        static float Distcance(Vector2 a, Vector2 b) { return Math::Sqrt((a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y)); }
+        static constexpr float DistcanceSquared(Vector2 a, Vector2 b) { return (a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y); }
+
+        static Vector2 Direction(Vector2 from, Vector2 to);
+        static RadiansF DirectionAngle(Vector2 from, Vector2 to);
+
+        static constexpr float Cross(Vector2 a, Vector2 b) { return a.x * b.y - a.y * b.x; }
+        static constexpr float Dot(Vector2 a, Vector2 b) { return a.x * b.x + a.y * b.y; }
+
+        static constexpr Vector2 FromAngle(RadiansF angle)
+        {
+            Vector2 vec;
+            Math::SinCos(vec.x, vec.y, angle);
+            return vec;
+        }
+
+        static constexpr Vector2 FromAngle(RadiansF angle, float length) { return FromAngle(angle) * length; }
+
+        static constexpr Vector2 Lerp(Vector2 from, Vector2 to, float t) { return from + (to - from) * t; }
+        static constexpr Vector2 LerpClamped(Vector2 from, Vector2 to, float t) { return from + (to - from) * Math::Clamp(t); } 
+        static Vector2 Slerp(Vector2 from, Vector2 to, float t);
+        static Vector2 MoveTowards(Vector2 current, Vector2 target, float maxDelta);
+
+        static constexpr Vector2 Project(Vector2 vec, Vector2 projectOnto) { return projectOnto * (Dot(vec, projectOnto) / projectOnto.LengthSquared()); }
+        static constexpr Vector2 ProjectNormalized(Vector2 vec, Vector2 projectOnto) { return projectOnto * Dot(vec, projectOnto); }
+
+        static constexpr Vector2 Reflect(Vector2 vec, Vector2 normal) { return vec - 2.0f * Dot(vec, normal) * normal; }
+
+        static constexpr bool IsEqualApproximetly(Vector2 a, Vector2 b) { return Math::IsEqualApproximetly(a.x, b.x) && Math::IsEqualApproximetly(a.y, b.y); }
+
+        constexpr Vector2 operator+() const { return Vector2(+x, +y); }
+        constexpr Vector2 operator-() const { return Vector2(-x, -y); }
 
         constexpr Vector2 operator+(Vector2 a) const { return Vector2(a.x + x, a.y + y); }
         constexpr Vector2 operator+(float a) const { return Vector2(a + x, a + y); }
@@ -79,7 +144,7 @@ namespace ByteEngine
         }
 
         constexpr Vector2 operator/(Vector2 a) const { return Vector2(x / a.x, y / a.y); }
-        constexpr Vector2 operator/(float s) const { return Vector2(x / s, y / s); }        
+        constexpr Vector2 operator/(float s) const { return Vector2(x / s, y / s); }
 
         constexpr Vector2& operator/=(Vector2 a)
         {
@@ -95,8 +160,20 @@ namespace ByteEngine
             return *this;
         }
 
-        float& operator[](int32 index) { return data[index]; }
-        float operator[](int32 index) const { return data[index]; }
+        constexpr bool operator==(Vector2 other) const { return x == other.x && y == other.y; }
+        constexpr bool operator!=(Vector2 other) const { return !(*this == other); }
+
+        float& operator[](int32 index) 
+        {
+            assert(index >= 0 && index < 2);
+            return data[index]; 
+        }
+
+        float operator[](int32 index) const 
+        {
+            assert(index >= 0 && index < 2);
+            return data[index];
+        }
     };
 
     static inline constexpr Vector2 Zero(0.0f);
---
+++ ByteEngineD3D11/Code/Source/Core/Math/Vector2.cpp
@@ -0,0 +1,86 @@
+﻿#include "Math/Vector2.h"
+
+namespace ByteEngine::Math
+{
+    void Vector2::Normalize()
+    {
+        float length = LengthSquared();
+
+        if (length != 0.0f)
+        {
+            length = Math::Sqrt(length);
+            x /= length;
+            y /= length;
+        }
+    }
+
+    Vector2 Vector2::Normalized() const
+    {
+        Vector2 copy = *this;
+        copy.Normalize();
+        return copy;
+    }
+
+    void Vector2::LimitLength(float maxLength)
+    {
+        float currentLength = LengthSquared();
+
+        if (currentLength > maxLength * maxLength)
+        {
+            currentLength = Math::Sqrt(currentLength);
+            *this *= maxLength / currentLength;
+        }
+    }
+
+    RadiansF Vector2::AngleBetween(Vector2 from, Vector2 to)
+    {
+        float cross = Cross(from, to);
+        float dot = Dot(from, to);
+        return Math::Atan2(cross, dot);
+    }
+
+    RadiansF Vector2::UnsigedAngleBetween(Vector2 from, Vector2 to) { return Math::Abs(AngleBetween(from, to)); }
+
+    Vector2 Vector2::Direction(Vector2 from, Vector2 to)
+    {
+        Vector2 dir = to - from;
+        dir.Normalize();
+        return dir;
+    }
+
+    RadiansF Vector2::DirectionAngle(Vector2 from, Vector2 to)
+    {
+        Vector2 dir = to - from;
+        dir.Normalize();
+        return Math::Atan2(dir.x, dir.y);
+    }
+
+    Vector2 Vector2::Slerp(Vector2 from, Vector2 to, float t)
+    {
+        assert(t >= 0.0f && t <= 1.0f);  
+
+        float startLength = from.LengthSquared();
+        float endLength = to.LengthSquared();
+
+        if (startLength == 0.0f || endLength == 0.0f)
+            return Lerp(from, to, t);
+
+        startLength = Math::Sqrt(startLength);
+        float resultLength = Math::Lerp(startLength, Math::Sqrt(endLength), t);
+        float angle = AngleBetween(from, to);        
+
+        from.RotateBy(angle * t);
+        return from * (resultLength / startLength);
+    }
+
+    Vector2 Vector2::MoveTowards(Vector2 current, Vector2 target, float maxDelta)
+    {
+        Vector2 vd = target - current;
+        float length = vd.Length();
+
+        if (length <= maxDelta || length < Math::Epsilon)
+            return target;
+        else
+            return current + vd / length * maxDelta;
+    }
+}
